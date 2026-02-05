64c106e496ec1f4207291aa0e51401c4edc05908
Legion <legion26042010@gmail.com>
2026-02-06 00:24:49 +0300

    temporarily removed OTP sending

--- backend/internal/service/auth.go
+++ backend/internal/service/auth.go
@@ -3,15 +3,9 @@ package service
 import (
 	"backend/internal/model"
 	"backend/internal/repository"
-	"backend/pkg/config"
 	"backend/pkg/utils"
-	"bytes"
-	"encoding/json"
 	"errors"
-	"fmt"
-	"io"
 	"log"
-	"net/http"
 )
 
 type authService struct {
@@ -30,42 +24,42 @@ func NewAuthService(r repository.AuthRepository) AuthService {
 
 func (s *authService) SendOtp(phone model.PhoneRequest) error {
 	code := utils.GenerateOTP()
-	reqBody := model.SmsRequest{
-		SenderID:   "MsgRush",
-		Recipients: []string{phone.Phone},
-		Message:    fmt.Sprintf("Your code is: %s", code),
-	}
+	//reqBody := model.SmsRequest{
+	//	SenderID:   "Yakjo",
+	//	Recipients: []string{phone.Phone},
+	//	Message:    fmt.Sprintf("Your code is: %s", code),
+	//}
+	//log.Println("Code: ", code)
+	//
+	//bodyBytes, _ := json.Marshal(reqBody)
+	//
+	//url := config.GetEnv("msgRushUrl")
+	//
+	//req, err := http.NewRequest(http.MethodPost, url, bytes.NewBuffer(bodyBytes))
+	//if err != nil {
+	//	return err
+	//}
+	//apiKey := config.GetEnv("msgRushApiKey")
+	//req.Header.Set("X-API-Key", apiKey)
+	//req.Header.Set("Content-Type", "application/json")
+	//
+	//client := &http.Client{}
+	//resp, err := client.Do(req)
+	//if err != nil {
+	//	fmt.Printf("Ошибка при отправке запроса: %v\n", err)
+	//	return err
+	//}
+	//defer resp.Body.Close()
+	//
+	//body, _ := io.ReadAll(resp.Body)
+	//if resp.StatusCode != http.StatusOK {
+	//	return errors.New(string(body))
+	//}
 	err := s.r.SaveOtp(phone.Phone, utils.GenerateHashFromPassword(code)) // сохраняем код в БД
-	log.Println("Code: ", code)
-
-	bodyBytes, _ := json.Marshal(reqBody)
-
-	url := config.GetEnv("msgRushUrl")
-
-	req, err := http.NewRequest(http.MethodPost, url, bytes.NewBuffer(bodyBytes))
 	if err != nil {
 		return err
 	}
-	apiKey := config.GetEnv("msgRushApiKey")
-	req.Header.Set("X-API-Key", apiKey)
-	req.Header.Set("Content-Type", "application/json")
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		fmt.Printf("Ошибка при отправке запроса: %v\n", err)
-		return err
-	}
-	defer resp.Body.Close()
-
-	body, _ := io.ReadAll(resp.Body)
-	if resp.StatusCode != http.StatusOK {
-		return errors.New(string(body))
-	}
-	err = s.r.SaveOtp(phone.Phone, utils.GenerateHashFromPassword(code)) // сохраняем код в БД
-	if err != nil {
-		return err
-	}
+	log.Println("Verify OTP:", phone.Phone)
 	return nil
 }
 
