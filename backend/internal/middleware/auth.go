bfd119ee8ffc2348ad4b263417c2171addb81168
Legion <legion26042010@gmail.com>
2026-02-06 19:33:07 +0300

    add auth middleware

---
+++ backend/internal/middleware/auth.go
@@ -0,0 +1,27 @@
+package middleware
+
+import (
+	"backend/pkg/utils"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func JWTAuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		token, err := c.Cookie("token")
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "No access token"})
+			c.Abort()
+			return
+		}
+		claims, err := utils.VerifyJwtToken(token)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid or expired token"})
+			c.Abort()
+			return
+		}
+		c.Set("user_id", claims.UserID)
+		c.Next()
+	}
+}
