6d677197a65352c3fda4d38f6b813497c3d50814
legion <legion26042010@gmail.com>
2026-02-10 14:05:29 +0500

    change jwt logic

--- backend/pkg/utils/jwt.go
+++ backend/pkg/utils/jwt.go
@@ -3,20 +3,16 @@ package utils
 import (
 	"backend/pkg/config"
 	"errors"
-	"fmt"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
 )
 
-// CustomClaims описывает структуру данных внутри токена
 type CustomClaims struct {
 	UserID int `json:"user_id"`
-	// Можно добавить любые поля: Role, Email и т.д.
 	jwt.RegisteredClaims
 }
 
-// Generate создает новый токен для пользователя
 func GenerateJwtToken(userID int, exp time.Duration) (string, error) {
 	claims := CustomClaims{
 		UserID: userID,
@@ -30,28 +26,11 @@ func GenerateJwtToken(userID int, exp time.Duration) (string, error) {
 	return token.SignedString([]byte(config.GetEnv("JWT_SECRET")))
 }
 
-// Verify проверяет токен и возвращает данные (claims)
-func VerifyJwtToken(tokenString string) (*CustomClaims, error) {
-	token, err := jwt.ParseWithClaims(
-		tokenString,
-		&CustomClaims{},
-		func(token *jwt.Token) (interface{}, error) {
-			// Проверяем алгоритм подписи
-			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-				return nil, fmt.Errorf("unexpected token signing method")
-			}
-			return []byte(config.GetEnv("JWT_SECRET")), nil
-		},
-	)
-
-	if err != nil {
-		return nil, fmt.Errorf("invalid token: %w", err)
+func VerifyJwtToken(key string) (*CustomClaims, error) {
+	claims := &CustomClaims{}
+	token, err := jwt.ParseWithClaims(key, claims, func(token *jwt.Token) (interface{}, error) { return []byte(config.GetEnv("JWT_SECRET")), nil })
+	if err != nil || !token.Valid {
+		return nil, errors.New("invalid token")
 	}
-
-	claims, ok := token.Claims.(*CustomClaims)
-	if !ok || !token.Valid {
-		return nil, errors.New("invalid token claims")
-	}
-
 	return claims, nil
 }
