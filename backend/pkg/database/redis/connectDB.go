5d3d0fd03c87355d4701e50fe9b4a86a89ee23d1
Jamoliddin <legion26042010@gmail.com>
2026-04-06 13:14:06 +0500

    fix redis conn

--- backend/pkg/database/redis/connectDB.go
+++ backend/pkg/database/redis/connectDB.go
@@ -1,6 +1,7 @@
 package redis
 
 import (
+	"backend/pkg/config"
 	"context"
 	"log"
 
@@ -12,9 +13,9 @@ var Client *redis.Client
 
 func ConnectDB() {
 	Client = redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
-		Password: "", // no password set
-		DB:       0,  // use default DB
+		Addr:     config.GetEnv("REDIS_DB_HOST"),
+		Password: config.GetEnv("REDIS_DB_PASSWORD"), // no password set
+		DB:       0,                                  // use default DB
 	})
 
 	if err := Client.Ping(ctx).Err(); err != nil {
